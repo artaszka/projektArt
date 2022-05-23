d05cc1efed679bb551e855904c1f3288ed60bf05
Artur <31184382+artaszka@users.noreply.github.com>
2022-05-23 21:34:36 +0200

    Zadanie 2 update

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ def zadanie1():
 @app.get("/method", status_code=200)
 def zadanie2():
     return {"method": "GET"}
+
+@app.post("/method", status_code=201)
+def zadanie2():
+    return {"method": "POST"}
+
+@app.put("/method", status_code=200)
+def zadanie2():
+    return {"method": "PUT"}
+
+@app.options("/method", status_code=200)
+def zadanie2():
+    return {"method": "OPTIONS"}
+
+@app.delete("/method", status_code=200)
+def zadanie2():
+    return {"method": "DELETE"}
