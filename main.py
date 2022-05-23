64d056923b454bdc71145fac17983ddd34b219f2
Artur <31184382+artaszka@users.noreply.github.com>
2022-05-23 20:43:21 +0200

    Update main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
-    return {"message": "Hello World, matrix has you"}
+    # return {"message": "Hello World, matrix has you"}
+    return "message => Hello World, matrix has you"
